b11aecb7669166bf72fe951407e9eddc049abc26
pheever <phivos.phivou@gmail.com>
2021-01-17 13:26:28 +0200

    initial commit

---
+++ planner/index.js
@@ -0,0 +1,23 @@
+if (!WebAssembly.instantiateStreaming) {
+  // polyfill
+  WebAssembly.instantiateStreaming = async (resp, importObject) => {
+    const source = await (await resp).arrayBuffer();
+    return await WebAssembly.instantiate(source, importObject);
+  };
+}
+
+const go = new Go();
+
+let mod, inst;
+
+WebAssembly.instantiateStreaming(fetch("planner.wasm"), go.importObject).then(
+  async result => {
+    mod = result.module;
+    inst = result.instance;
+    await go.run(inst);
+  }
+);
+
+function jsAdd(x,y){
+  return x-y;
+}
