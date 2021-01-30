1c84156848f46bffaf15af0d170d653600d8e698
pheever <phivos.phivou@gmail.com>
2021-01-30 20:21:38 +0000

    major changes

--- planner/src/app/deductions/deductions.component.ts
+++ planner/src/app/deductions/deductions.component.ts
@@ -9,19 +9,26 @@ import { Employments, Employment } from '../contributions'
 
 export class DeductionsComponent implements OnInit {
 
-  @Input() anualIncome: number = 0
+  @Input() annualIncome: number = 0
   @Input() monthlyIncome: number = 0
 
+  @Input() providentFund: number = 0
 
   employmentTypes = Employments
   selectedStatus: Employment = Employments.filter(v => v.status == "employee")[0]
 
   tax: number = 0
   constructor() { }
-
+  numberWithCommas = (x: string) => x.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
   onSelect = (status: Employment) => this.selectedStatus = status
-  monthlyDeductions = () => Math.fround(this.monthlyIncome * (this.selectedStatus.si + this.selectedStatus.ghs) / 100);
-  anualDeductions = () => Math.fround(this.anualIncome * (this.selectedStatus.si + this.selectedStatus.ghs) / 100);
+  monthlyDeductions = () => (this.monthlyIncome * (this.selectedStatus.si + this.selectedStatus.ghs + this.providentFund) / 100);
+  monthlySI = () => (this.monthlyIncome * this.selectedStatus.si / 100);
+  monthlyGHS = () => (this.monthlyIncome * this.selectedStatus.ghs / 100);
+  monthlyPF = () => (this.monthlyIncome * this.providentFund / 100);
+  annualDeductions = () => (this.annualIncome * (this.selectedStatus.si + this.selectedStatus.ghs + this.providentFund) / 100);
+  annualSI = () => (this.annualIncome * this.selectedStatus.si / 100);
+  annualGHS = () => (this.annualIncome * this.selectedStatus.ghs / 100);
+  annualPF = () => (this.annualIncome * this.providentFund / 100);
 
   ngOnInit(): void {
   }
